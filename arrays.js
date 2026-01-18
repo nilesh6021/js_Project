bc16e047ce331e4d35ba18ade136cb538a4afc4b
Nilesh Patil <nileshpatil6021@gmail.com>
2026-01-18 10:27:03 +0530

    ME_QTRIPDYNAMIC_MODULE_ADVENTURE_DETAILS

--- arrays.js
+++ arrays.js
@@ -9,20 +9,51 @@
 //You can use various array methods to manipulate arrays, such as push(), pop(), shift(), unshift(), splice(), slice(), and more.
 
 //Example of array creation
-const fruits =[]//empty array
-fruits.push("apple");
-fruits.push("banana","orange","peach","grape","mango");
-console.log("Fruites:" ,fruits);
-console.log(fruits.length);
+// const fruits =[]//empty array
+// fruits.push("apple");
+// fruits.push("banana","orange","peach","grape","mango");
+// console.log("Fruites:" ,fruits);
+// console.log(fruits.length);
 
-console.log(fruits[5]);
+// console.log(fruits[5]);
 
-console.log(Object.keys(fruits));
-
-
- 
+// console.log(Object.keys(fruits));
 
 
+ const numberArray =[0,1,2,3,4,5];
+
+ const mixedArray =[0,'chikan',true,2.09,{name:"nilesh"},true];
+ console.log(numberArray);
+ console.log(mixedArray);
+
+//const musicGenres = ['Rock', 'Pop', 'Country'];
+
+//const musicGenres =new Array('Rock','Pop','Country')
+
+// const musicGenres =[];
+// musicGenres.push('Rock','Pop','Counrty');
 
 
+// const genreOne = 'Rock';
+// const genreTwo = 'Pop';
+// const genreThree = 'Country';
+// const musicGenres = [genreOne, genreTwo, genreThree];
+
+// console.log(musicGenres);
+
+// console.log(musicGenres[0]);
+// console.log(musicGenres[0]="Soul");
+// console.log(musicGenres);
+
+
+
+const musicGenres = [
+  ['Rock', 'Pop', 'Country'],
+  ['Soul', 'Hip Hop', 'Reggae'],
+  ['Folk', 'Classical', 'Electronic'],
+];
+
+console.log(musicGenres[1][2]);//reggae
+console.log(musicGenres[0][1]);//Pop
+console.log(musicGenres[2][1]);//classical
 // node arrays.js
